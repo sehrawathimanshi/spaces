891277c1375a0b7d25147822ac9dfa331e3a6953
hsehrawat3 <hsehrawat3@gmail.com>
2020-09-05 12:30:56 +0530

    Gzip compression

--- server.ts
+++ server.ts
@@ -7,13 +7,16 @@ import { join } from 'path';
 import { AppServerModule } from './src/main.server';
 import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
-
+import * as expressStaticGzip from 'express-static-gzip';
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
   const distFolder = join(process.cwd(), 'dist/space/browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
+  server.use('/', expressStaticGzip(distFolder, {
+    enableBrotli: true
+  }));
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
   server.engine('html', ngExpressEngine({
     bootstrap: AppServerModule,
